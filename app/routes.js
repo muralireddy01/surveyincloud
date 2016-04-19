e09845a6f04766df1adf2ab2d66f754712e52c13
ialberdic <ialberdic@gmail.com>
2016-04-19 01:00:26 +0100

    Updated

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,13 @@
 var db = require('../app/models/shortUrlModel');
 var utils = require('../config/utils');
 var express = require('express');
+var http = require('http');
+var request = require("request");
+var parseString = require('xml2js').parseString;
+var htmlparser = require("htmlparser");
+var sys         = require("util");
+var Q = require("q"); 
+var rp = require('request-promise');
 
 module.exports = function (app) {
 
@@ -19,7 +26,33 @@ module.exports = function (app) {
 
     app.post('/create', function (req, res) {
         var data = null;
+        var rawHtml = null;
 
+        var handler = new htmlparser.DefaultHandler(function (error, dom) {
+                         console.log(error);     
+
+               });
+               var parser = new htmlparser.Parser(handler);
+//console.log("PARSER", parser);
+//console.log(parser.parseComplete(rawHtml));
+//sys.puts(sys.inspect(handler.dom, false, null));
+
+        
+
+        rp("http://rss.betfair.com/RSS.aspx?format=html&sportID=1").then(function(response) { 
+            //console.log("RESPONSE", response)
+        //rawHtml = response;
+               
+            parser.parseComplete(response);
+       sys.puts(sys.inspect(handler.dom, false, null));       
+       // result = parseString(xml);
+    
+       });
+       
+
+
+       
+            
         if (req.body.url === '' || req.body.url === undefined) {
             return res.send(400);
         }
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,49 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $sce) {
 
-	$scope.tagline = 'The best framework...is it not?';	
+    $scope.tagline = 'The best framework...is it not?';
+   // $scope.result = result();
+    
+    $scope.trustUrl = function(url) {
+        var url = "www.google.com"
+        console.log("entra");
+        return $sce.trustAsResourceUrl(url);
+         
+    };
+    $scope.scoreQuestions = {
+        favourite: false,
+        known: false,
+        winner: false,
+        starting: false,
+        prop: false,
+        setNumber: false
+    };
+    $scope.stopSilly = {
+        
+    };
 
+   
+    
+    $scope.result = function () {
+        var question = 0;
+        console.log($scope.scoreQuestions);
+        if ($scope.scoreQuestions.setNumber === "UNO") {
+            
+            angular.forEach($scope.scoreQuestions, function(questions) {
+                console.log("Q", questions);
+                if (questions === true) {
+                    question++;
+                }
+                console.log("QUESTION", question);
+                
+            });
+            if (question >= 5) {
+                    console.log("CAMBIA FALSE");
+                    return true;  
+                } else {
+                    console.log("CAMBIA TRUE");
+                     return false;
+                }
+        };
+              
+    };
 });
